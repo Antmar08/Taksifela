1bf60f07c16cdf74af868e5ac53f5804bff3e021
benjam-code <benjam.wentin@gmail.com>
2026-02-12 14:18:40 +0200

    Add files via upload

---
+++ Taksifela-main/Kirjautuminen/actionpage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=0.2">
+  <title>Kirjautuminen epäonnistui</title>
+  <h1>Kirjautuminen epäonnistui</h1>
+  <li><a href="../suomi/kirjaudu.php" class="button">Palaa takaisin kirjautumissivulle</a></li>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<?php
+
+include "connect.php";
+
+// jos käyttäjä on tullut kirjaudu.php lomakkeen kautta
+if (isset($_POST['kayttajanimi'])) {
+
+    // estää SQL-hyökkäykset
+    $kayttajanimi = mysqli_real_escape_string($conn,$_POST['kayttajanimi']);
+    $salasana = mysqli_real_escape_string($conn,$_POST['salasana']);
+
+if ($kayttajanimi == NULL OR $salasana == NULL) {
+    header("location:../suomi/kirjaudu.php");
+} else {
+    $sql = "SELECT * FROM users WHERE username ='".$kayttajanimi."' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $hashedPwd = $row['password'];
+    if(password_verify($salasana, $hashedPwd)){
+        mysqli_query($conn, $sql);
+        $_SESSION["kayttajanimi"] = $kayttajanimi;
+        header ("location:../suomi/etusivu.php");
+        echo "Kirjautuminen onnistui";
+    } else {
+        echo "Kirjautuminen epäonnistui";
+        exit();
+    }
+}
+}
+
+
+?>
+
+
---
+++ Taksifela-main/Muokkaa/muokkaa.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="juttu.css" />
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<?php
+
+include "connect.php";
+
+$taksa =                    $_POST['taksa'];
+$alv =                      $_POST['alv'];
+$esimerkkimatka =           $_POST['esimerkkimatka'];
+$aloitusmaksu =             $_POST['aloitusmaksu'];
+$aloitusmaksumuina =        $_POST['aloitusmaksumuina'];
+$matka =                    $_POST['matka'];
+$aika =                     $_POST['aika'];
+$id =                       $_POST['id'];
+
+$sql = "UPDATE taksifela SET taksa='$taksa', alv='$alv', esimerkkimatka='$esimerkkimatka', aloitusmaksu='$aloitusmaksu', aloitusmaksumuina='$aloitusmaksumuina', matka='$matka', aika='$aika' WHERE id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Hinnaston tiedot muokattu onnistuneesti, <a href='../suomi/hinnasto.php'>Palaa takaisin</a>";
+
+} else {
+    echo "Virhe: " . $conn->error;
+}
+?>
