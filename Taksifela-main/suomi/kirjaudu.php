a21240c75fa74ad161a878d8bfca69deefd806fa
benjam-code <benjam.wentin@gmail.com>
2026-03-18 09:43:16 +0200

    Add files via upload

---
+++ Taksifela-main/suomi/kirjaudu.php
@@ -0,0 +1,118 @@
+<?php
+include "../Kirjautuminen/connect.php";
+?>
+
+<!-- Tämä ottaa ctrl napin toiminnon pois käytöstä eli vaikeampi esim saada nettisivun koodia ja ei voi tallentaa sivua suoraan filenä -->
+<script>
+ document.addEventListener("keydown", function (event){
+
+  if (event.ctrlKey){
+
+     event.preventDefault();
+
+  }
+
+  if(event.keyCode == 123){
+
+     event.preventDefault();
+
+  }
+
+})
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+    <!--- TYYLIN LINKKI JA SCRIPT -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taksifela kirjautuminen</title>
+    <link rel="stylesheet" href="../style css/kirjaudu.css">
+</head>
+<body>
+
+    <div class="site">
+
+     <header>
+
+    <?php
+    if(isset($_SESSION['kayttajanimi'])) {
+    echo "Kirjautunut käyttäjä: " . $_SESSION['kayttajanimi'];
+    }
+    ?>
+
+        <a href="etusivu.php"><img id="logo" src="../kuvat/logo.png" alt="logo"></a>
+
+        <a href="tel: +358 040 3765890"><img id="pnumero" src="../kuvat/Pnumero.png" alt="pnumero"></a>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <ul class="nav" id="topnav">
+            <a href="etusivu.php">Etusivu</a>
+            <a href="hinnasto.php">Hinnasto</a>
+            <a href="palvelut.php">Kelataksi</a>
+            <a href="yhteystiedot.php">Ota yhteyttä</a>
+
+            <!-- Kirjaudu ulos nappi joka näkyy vain sillon kun olet kirjautunut -->
+            <?php
+            if (isset($_SESSION['kayttajanimi'])) {
+            ?>  
+            <a href="../Kirjautuminen/logout.php">Kirjaudu ulos</a>
+            <?php
+            }
+            ?>
+
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>  
+            </a>
+
+            <script src="../style css/nav.js"></script>
+        </ul>
+    </header>
+    
+    <main>
+
+<h1>Kirjaudu adminiksi</h1>
+
+<form class="fela-form" method="POST" action="../Kirjautuminen/actionpage.php">
+  <input type="hidden" name="access_key" value="">
+
+  <label>
+    <span>Käyttäjänimi</span>
+    <input type="text" name="kayttajanimi" id="kayttajanimi" required>
+  </label>
+
+  <label>
+    <span>Salasana</span>
+    <input type="password" name="salasana" id="salasana" required>
+  </label>
+
+  <button type="submit">Kirjaudu</button>
+ 
+</form>
+
+<!-- Tämä näyttää "vaihda käyttäjätietoja" napin vain kirjautuneelle -->
+ <?php
+if (isset($_SESSION['kayttajanimi'])) {
+?>  
+  <a href="rekisteroidy.php"><button>Vaihda käyttäjätietoja</button></a>
+<?php
+}
+?>
+
+</main>
+
+    <footer>
+    <p1>&copyTaksi Fela</p1><?php date_default_timezone_set('Europe/Helsinki'); echo date(" Y"); ?><br>
+    <a href="kirjaudu.php" id="admin">Kirjaudu Adminiksi</a>
+    </footer>
+
+</div>
+
+</body>
+
+</html>
+
+
